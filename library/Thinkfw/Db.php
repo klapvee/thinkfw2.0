e2a49ac83e4d5cdeff2a29ab9cba84ea5b330fac
willem-daems <willem.daems@rtvnh.nl>
2012-11-25 14:52:44 +0100

    Throw exception on failure

--- library/Thinkfw/Db.php
+++ library/Thinkfw/Db.php
@@ -2,16 +2,25 @@
 
 namespace Thinkfw;
 
+
 class Db
 {
     private $adapter;
 
     public function __construct($adapter, $host, $username, $password)
     {
+        try {
+            $adapterName = '\Thinkfw\Db\Adapters\\'.ucfirst($adapter);
 
-        $adapterName = '\Thinkfw\Db\Adapters\\'.ucfirst($adapter);
-        $this->adapter = new $adapterName();
+            $this->adapter = new $adapterName(
+                    $host,
+                    $username,
+                    $password
+                );
+        } catch (Exception $e) {
+            echo "Database instantiation failed: " . $e->getMessage();
+        }
 
-        echo $this->adapter->identify();
+        return $this->adapter;
     }
 }
