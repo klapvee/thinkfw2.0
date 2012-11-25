857fbe26ed709c24ff6244ecb012f649c2590416
willem-daems <willem.daems@rtvnh.nl>
2012-11-25 14:50:40 +0100

    Added comments, rendering content, added view object

--- library/Thinkfw/Application/Bootstrap.php
+++ library/Thinkfw/Application/Bootstrap.php
@@ -9,11 +9,13 @@
  * @subpackage Bootstrap;
  * @author WillemDaems
  *
+ * @todo put contents of view directly in layout
+ *
  */
 
 namespace Thinkfw\Application;
 
-use \Front\Controllers as Controllers;
+use \Front\Cms\Controllers as Controllers;
 
 class Bootstrap
 {
@@ -48,21 +50,37 @@ class Bootstrap
      */
     public static function run(\Thinkfw\Application $application)
     {
-
-
+        // set a new application
         self::$application = $application;
 
+        // get parsed config object
         self::$config = new \Thinkfw\Config('/application/Config/site.ini');
+
+        // tell the application to start
         self::$application->run();
+
+        // init a new controller corresponding to request
         self::$controller = new Controllers\Index;
 
+        // start object output caching
         ob_start();
 
-
+        // perform the action corresponding to request
         self::$controller->Action();
 
+        // set view location from which the view will be loaded from
+        self::$controller->view->setViewLocation('Cms/Views/html/index/index.phtml');
+
+        // render and return the html output from the view
+        $view = self::$controller->view->render();
+
+        // parse the layout html
         $content = ob_get_contents();
 
+        // add the view contents
+        $content .= $view;
+
+        // clean
         ob_end_clean();
 
         echo $content;
@@ -86,7 +104,7 @@ class Bootstrap
     public function getDatabase() {
 
     }
-    
+
     public static function getConfig()
     {
         return self::$config;
