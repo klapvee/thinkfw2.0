688b280854428c684a9b221d02df431cdb0dbdb7
willem-daems <willem.daems@rtvnh.nl>
2012-12-07 22:08:38 +0100

    Default form parsers for cms modules

--- application/Cms/Controllers/Module.php
+++ application/Cms/Controllers/Module.php
@@ -6,6 +6,7 @@ use Thinkfw\Controller\Front;
 use Thinkfw\Application;
 use Thinkfw\Xml;
 
+use APP\Cms\Forms\Module as ModuleForm;
 use APP\Cms\Models\Module as ModuleModel;
 
 class Module extends Front
@@ -31,7 +32,7 @@ class Module extends Front
         $module = $this->model->getModule($id);
         $data = $this->model->getData($module);
 
-        $this->view->id = (int) $module['id'];
+        $this->view->moduleId = (int) $module['id'];
         $this->view->name = $module['name'];
         $this->view->data = $data;
 
@@ -44,19 +45,47 @@ class Module extends Front
         $id = $this->getParam(3);
 
         $module = $this->model->getModule($id);
-        $data = $this->model->getData($module);
-
         $xml = new Xml($module['definition']);
+
+        $form = new ModuleForm;
+        $form->init($xml->getData());
 
         if (!empty($_POST)) {
 
-            $this->model->insertData($xml->getData(), $module, $_POST);
-            Application::redirect('/module/overview/'.$id);
+            // set the parameters in the form
+            $form->setParameters($this->getParams());
+
+            // do validation check
+            if ( $form->validate() === true) {
+
+                $this->model->insertRecord($xml->getData(), $module, $_POST);
+                Application::redirect('/module/overview/'.$id);
+            }
         }
 
-        $this->view->id = (int) $module['id'];
+
+
+        $this->view->form = $form->render();
+
+        $this->view->moduleId = (int) $module['id'];
         $this->view->name = $module['name'];
 
         $this->view->data = $xml->getData();
+    }
+
+    public function edit()
+    {
+        $moduleId = $this->getParam(3);
+        $id = $this->getParam(4);
+
+        $module = $this->model->getModule($moduleId);
+        $xml = new Xml($module['definition']);
+
+        if (!empty($_POST)) {
+            $this->model->updateRecord($xml->getData(), $module, $_POST, $id);
+        }
+
+        $this->view->data = $this->model->getRecord($module, $id);
+        $this->view->xml = $xml->getData();
     }
 }
